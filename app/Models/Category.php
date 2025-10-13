a46dc901c7a3fcf892a9c41a19512023cc40ecf8
rhecustein <bintangwijaya18@gmail.com>
2025-10-13 20:28:27 +0700

    fix

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Traits\BelongsToTenant;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
